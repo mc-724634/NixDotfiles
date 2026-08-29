4c01e68549fae89a1b6402bf93f167f0ef43ba09
mc <mcanida09@gmail.com>
2026-08-29 07:01:20 -0400

    Fixed browser plasma intigration fr

--- home.nix
+++ home.nix
@@ -410,5 +410,8 @@ in
     };
   };
 
+  xdg.configFile."net.imput.helium/NativeMessagingHosts/org.kde.plasma.browser_integration.json".source =
+  "${pkgs.kdePackages.plasma-browser-integration}/etc/chromium/native-messaging-hosts/org.kde.plasma.browser_integration.json";
+
   programs.home-manager.enable = true;
 }
